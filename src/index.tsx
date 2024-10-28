f85dd93aa1eea2ae13fd4b5e198a40bdc2a9e7d9
BiswadeepMazumder <biswadeepmazumder96@gmail.com>
2024-10-28 16:38:20 -0400

    Add frontend project files to frontend branch

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import CoursePage from './Course'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+    {/* <Login/>
+    <Dashboard />
+    <CoursePage /> */
+    <App/>}
+    </ChakraProvider>
+
+
   </React.StrictMode>
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
